c89be2cd1c13780f72b1540e880372c855b3bb6e
Frank Hundley <mariner105@msn.com>
2023-10-01 10:17:22 -0700

    True up to course code

--- src/main/java/dev/lydtech/tracking/TrackingConfiguration.java
+++ src/main/java/dev/lydtech/tracking/TrackingConfiguration.java
@@ -41,6 +41,7 @@ public class TrackingConfiguration {
         config.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JsonDeserializer.class);
         config.put(JsonDeserializer.VALUE_DEFAULT_TYPE, DispatchPreparing.class);
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+
         return new DefaultKafkaConsumerFactory<>(config);
     }
 
--- src/test/java/dev/lydtech/tracking/integration/DispatchTrackingIntegrationTest.java
+++ src/test/java/dev/lydtech/tracking/integration/DispatchTrackingIntegrationTest.java
@@ -1,6 +1,7 @@
 package dev.lydtech.tracking.integration;
 
 import dev.lydtech.dispatch.message.DispatchPreparing;
+import dev.lydtech.dispatch.message.TrackingStatusUpdated;
 import dev.lydtech.tracking.TrackingConfiguration;
 import dev.lydtech.tracking.util.TestEventData;
 import lombok.extern.slf4j.Slf4j;
@@ -19,7 +20,6 @@ import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.kafka.test.utils.ContainerTestUtils;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.support.MessageBuilder;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.concurrent.TimeUnit;
@@ -31,16 +31,16 @@ import static org.hamcrest.Matchers.equalTo;
 
 @Slf4j
 @SpringBootTest(classes = {TrackingConfiguration.class})
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 @ActiveProfiles("test")
 @EmbeddedKafka(controlledShutdown = true)
 public class DispatchTrackingIntegrationTest {
 
     private final static String DISPATCH_TRACKING_TOPIC = "dispatch.tracking";
+
     private final static String TRACKING_STATUS_TOPIC = "tracking.status";
 
     @Autowired
-    private KafkaTemplate kafkaTemplate;
+    private KafkaTemplate<String, Object> kafkaTemplate;
 
     @Autowired
     private EmbeddedKafkaBroker embeddedKafkaBroker;
@@ -53,42 +53,39 @@ public class DispatchTrackingIntegrationTest {
 
     @Configuration
     static class TestConfig {
-
         @Bean
         public KafkaTestListener testListener() {
             return new KafkaTestListener();
         }
     }
 
-    /**
-     * Use this receiver to consume messages from the outbound topics.
-     */
     public static class KafkaTestListener {
         AtomicInteger trackingStatusCounter = new AtomicInteger(0);
 
-        @KafkaListener(groupId = "KafkaIntegrationTest", topics = TRACKING_STATUS_TOPIC)
-        void receiveDispatchPreparing(@Payload DispatchPreparing payload) {
+        @KafkaListener(groupId = "kafkaIntegrationTest", topics = TRACKING_STATUS_TOPIC)
+        void receiveTrackingStatus(@Payload TrackingStatusUpdated payload) {
             log.debug("Received TrackingStatus: " + payload);
             trackingStatusCounter.incrementAndGet();
         }
+
     }
 
     @BeforeEach
     public void setUp() {
         testListener.trackingStatusCounter.set(0);
 
-        // Wait until the partitions are assigned.
-        registry.getListenerContainers().stream().forEach(container ->
+        registry.getListenerContainers().forEach(container ->
                 ContainerTestUtils.waitForAssignment(container, embeddedKafkaBroker.getPartitionsPerTopic()));
     }
 
     @Test
-    public void testDispatchTrackingFlow() throws Exception {
+    public void testDispatchTrackingFlow() throws Exception{
         DispatchPreparing dispatchPreparing = TestEventData.buildDispatchPreparingEvent(randomUUID());
         sendMessage(DISPATCH_TRACKING_TOPIC, dispatchPreparing);
 
         await().atMost(3, TimeUnit.SECONDS).pollDelay(100, TimeUnit.MILLISECONDS)
                 .until(testListener.trackingStatusCounter::get, equalTo(1));
+
     }
 
     private void sendMessage(String topic, Object data) throws Exception {
@@ -98,3 +95,4 @@ public class DispatchTrackingIntegrationTest {
                 .build()).get();
     }
 }
+
